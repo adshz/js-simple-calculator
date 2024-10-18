acd3798207037c687f90803ddfc00a33125058d2
shenghongzhong <howdavidlearncoding@gmail.com>
2024-10-18 19:25:15 +0100

    Refactor is done

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 const readline = require("readline-sync");
-const msg_list = require("./calculator_messages.json");
+const msgList = require("./calculator_messages.json");
 
 function  programPrompt(msg) {
   console.log(`=> ${msg}`);
@@ -9,47 +9,46 @@ function  isInvalid(num) {
   return (Number.isNaN(Number(num)) || num.trimStart() === "");
 }
 
-function  calculation() {
-
-  programPrompt(msg_list["prompt1"]);
-  let number1 = readline.question();
-  
-  while (isInvalid(number1)) {
-    programPrompt(msg_list["warn"]);
-    number1 = readline.question();
+function  getOperand(prompt) {
+  programPrompt(msgList[prompt]);
+  let num = readline.question();
+  while (isInvalid(num)) {
+    programPrompt(msgList["warn"]);
+    num = readline.question();
   }
-  number1 = Number(number1);
+  num = Number(num);
+  return (num);
+}
 
-  programPrompt(msg_list["prompt2"]);
-  let number2 = readline.question();
-  while (isInvalid(number2)) {
-    programPrompt(msg_list["warn"]);
-    number2 = readline.question();
-  }
-  number2 = Number(number2);
-
-  programPrompt(msg_list["confirm1"] + ` ${number1} and ${number2}\n`);
-  programPrompt(msg_list["operation"]);
+function  getOperation(prompt) {
   let opt = readline.question();
-
   while (!["1", "2", "3", "4"].includes(opt)) {
-    programPrompt(msg_list["warn"]);
+    programPrompt(msgList[prompt]);
     opt = readline.question();
   }
-
-  let msg = "";
-  if (opt === "1") {
-    msg += "Addition";
-  } else if (opt === "2") {
-    msg += "Subtraction";
-  } else if (opt === "3") {
-    msg += "Multification";
-  } else if (opt === "4") {
-    msg += "Division";
-  }
-  programPrompt(msg_list["confirm2"] + ` ${msg}\n`);
-
   opt = Number(opt);
+  return (opt);
+}
+
+function  confirmInput(opt = undefined, num1 = undefined, num2 = undefined) {
+  if (typeof num1 === 'undefined' && typeof num2 === 'undefined') {
+    let msg = "";
+    if (opt === 1) {
+      msg += "Addition";
+    } else if (opt === 2) {
+      msg += "Subtraction";
+    } else if (opt === 3) {
+      msg += "Multification";
+    } else if (opt === 4) {
+      msg += "Division";
+    }
+    programPrompt(msgList["confirm1"] + ` ${msg}\n`);
+  } else {
+    programPrompt(msgList["confirm2"] + ` ${num1} and ${num2}\n`);
+  }
+}
+
+function  calculation(number1, opt, number2) {
   let output;
   switch (opt) {
     case 1:
@@ -65,15 +64,25 @@ function  calculation() {
       output = number1 / number2;
       break;
   }
-  return (programPrompt(msg_list["output"] + ` ${output}\n`));
+  return (output);
 }
 
-programPrompt(msg_list["welcome"]);
+function  calculator() {
+  let number1 = getOperand("prompt1");
+  let number2 = getOperand("prompt2");
+  confirmInput(undefined, number1, number2);
+  programPrompt(msgList["operation"]);
+  let opt = getOperation("warn");
+  confirmInput(opt);
+  let output = calculation(number1, opt, number2);
+  return (programPrompt(msgList["output"] + ` ${output}\n`));
+}
+
+programPrompt(msgList["welcome"]);
 let again;
-while (1)
-{
-  calculation();
-  again = readline.question(msg_list["again"]);
+while (1) {
+  calculator();
+  again = readline.question(msgList["again"]);
   if (again.trimStart()[0] !== "y") break;
 }
-programPrompt(msg_list["end"]);
+programPrompt(msgList["end"]);
